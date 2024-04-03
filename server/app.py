2c9cb9965c233f78633487b4c0e7310dccc9e602
Ta'Keya McFadden <takeyamcfadden@gmail.com>
2024-04-03 12:16:18 -0400

    added requirements.txt

--- server/app.py
+++ server/app.py
@@ -1,22 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from models import User, Game, Score
+from resources.user import UserResource
+from resources.game import GameResource
+from resources.score import ScoreResource
 
 # Initialize app and setup database
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
-# Import models
-from models import User, Game, Score
-
 # Import API resources
-from resources.user import UserResource
-from resources.game import GameResource
-from resources.score import ScoreResource
-
-# Setup API
 api = Api(app)
 
 # Add resources to API
@@ -24,5 +20,23 @@ api.add_resource(UserResource, '/users')
 api.add_resource(GameResource, '/games')
 api.add_resource(ScoreResource, '/scores')
 
+# Define routes for handling HTTP requests
+@app.route('/')
+def index():
+    return '<h1>Project Server</h1>'
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = User(username=data['username'], email=data['email'])
+    db.session.add(user)
+    db.session.commit()
+    return user.to_dict(), 201
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
