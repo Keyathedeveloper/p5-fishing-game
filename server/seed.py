8e07b0e99d64b83c2d70422ff405c560d94f45b8
Ta'Keya McFadden <takeyamcfadden@gmail.com>
2024-04-14 14:16:47 -0400

    modified seed.py to include sample seed data for score and highscore.

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,5 @@
 from app import app, db
-from models import User
+from models import User, Score, HighScore
 
 def seed_users():
     try:
@@ -15,8 +15,29 @@ def seed_users():
             db.session.add(user2)
             db.session.commit()
             print("Sample users seeded successfully.")
+
+            # Create sample scores
+            score1 = Score(user=user1, score_value=10)
+            score2 = Score(user=user2, score_value=20)
+
+            # Add scores to the database
+            db.session.add(score1)
+            db.session.add(score2)
+            db.session.commit()
+            print("Sample scores seeded successfully.")
+
+            # Create sample high scores
+            high_score1 = HighScore(user=user1, score_value=100)
+            high_score2 = HighScore(user=user2, score_value=200)
+
+            # Add high scores to the database
+            db.session.add(high_score1)
+            db.session.add(high_score2)
+            db.session.commit()
+            print("Sample high scores seeded successfully.")
+
     except Exception as e:
-        print("Error seeding sample users:", e)
+        print("Error seeding sample data:", e)
         db.session.rollback()
 
 if __name__ == '__main__':
